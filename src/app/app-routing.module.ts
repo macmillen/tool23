81a83cc92d876a612d5b9fb312dbb1a5776ad4b9
FlatBit <compeso@web.de>
2019-06-19 12:42:19 +0200

    item update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: '', redirectTo: 'item-detail', pathMatch: 'full' },
 	{ path: 'home', loadChildren: './home/home.module#HomePageModule' },
 	{
 		path: 'account-view',
 		loadChildren:
 			'./user/account-view/account-view.module#AccountViewPageModule'
-	}
+	},
+  { path: 'item-detail', loadChildren: './item/item-detail/item-detail.module#ItemDetailPageModule' }
 ];
 
 @NgModule({
---
+++ src/app/item/item-detail/item-detail.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Item } from '../../models/item.model';
+import { Address } from 'src/app/models/address.model';
+
+@Component({
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.page.html',
+  styleUrls: ['./item-detail.page.scss'],
+})
+export class ItemDetailPage {
+
+  item: Item;
+
+  constructor() {
+    this.item = new Item('Hans', 'Bierdose',
+    'Ein kühles Bier zum Feierabend', new Date(),
+    'https://dyn0.media.forbiddenplanet.com/products/3948460c.jpg.jpg',
+    new Address('Frankfurterstraße', '75', '35392', 'Gießen'));
+  }
+
+
+
+}
