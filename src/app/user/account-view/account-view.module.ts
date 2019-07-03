095e04d638506c396862bd7c8066d9249a6aae76
Christoph Peil <3v9_56sm2oe8hrl1@byom.de>
2019-07-03 13:51:17 +0200

    universal footer added

--- src/app/user/account-view/account-view.module.ts
+++ src/app/user/account-view/account-view.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { AccountViewPage } from './account-view.page';
+import { FooterComponentModule} from '../../footer/footer.component.module';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FooterComponentModule
   ],
   declarations: [AccountViewPage]
 })
