f2f4ececa08bc3183862c5d559f36088e63c92bd
Christoph Peil <3v9_56sm2oe8hrl1@byom.de>
2019-07-09 17:50:23 +0200

    Creating new Item Service, and modifiy item model to work with server item model

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -2,24 +2,34 @@ import { ThrowStmt } from '@angular/compiler';
 import { Address } from './address.model';
 
 export class Item {
-    userID: string;
-    title: string;
-    description: string;
-    creationDate: Date;
-    imageURL: string;
-    address: Address;
-    tags: string[];
+  userID: string;
+  title: string;
+  description: string;
+  creationDate: Date;
+  imageURL: string;
+  address: {
+      zip: string,
+      street: string,
+      houseNumber: string,
+      city: string
+  }
+  tags: string[];
 
     constructor(
         userID?: string, title?: string, description?: string,
-        creationDate?: Date, imageURL?: string, address?: Address, tags?: string[]) {
+        creationDate?: Date, imageURL?: string, 
+        zip?: string, street?: string, houseNumer?: string, city?: string,
+        tags?: string[]) {
 
         this.userID = userID;
         this.title = title;
         this.description = description;
         this.creationDate = creationDate;
         this.imageURL = imageURL;
-        this.address = address;
         this.tags = tags;
+        this.address.zip = zip;
+        this.address.street = street;
+        this.address.houseNumber = houseNumer;
+        this.address.city = city;
       }
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,7 +1,35 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { SERVER_URL } from '../../environments/environment';
+import { Item } from '../models/item.model';
 
 @Injectable({ providedIn: 'root' })
 
 export class ItemService {
+
+    async createItem(itemIn: Item) {
+        const res = await axios.post(SERVER_URL + '/api/item', {
+            item: itemIn
+        });
+        return res;
+    }
+
+    async getItem(ID: string) {
+        //Should return one item
+        const res = await axios.get(SERVER_URL + '/api/item/' + ID);
+        return res;
+    }
+
+    async getAllItems() {
+        //Should return Array full of Items
+        const res = await axios.get(SERVER_URL + '/api/items/');
+        return res;
+    }
+
+    async deleteItem(ID: string) {
+        //Should return one item
+        const res = await axios.delete(SERVER_URL + '/api/item/' + ID);
+        return res;
+    }
 
 }
