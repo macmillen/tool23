ddb61d3df9148a92b2aac7d7c3a37b7f6b595056
Milan Jaritz <milan.jaritz@mni.thm.de>
2019-07-10 15:17:53 +0200

    delete imageURL field

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -7,7 +7,6 @@ export interface Item {
     status: 'disabled' | 'active';
     description: string;
     creationDate: Date;
-    imageUrl: string;
     address: Address;
     tags: string[];
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -8,5 +8,4 @@ export interface User {
     username: string;
     address: Address;
     password: string;
-    imageURL?: string;
 }
