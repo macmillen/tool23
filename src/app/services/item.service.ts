0c5c89fe484eb81a239d0274962441a0707006f6
Christoph Peil <3v9_56sm2oe8hrl1@byom.de>
2019-07-31 12:43:04 +0200

    Added comments for user and item service

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -9,30 +9,65 @@ export class ItemService {
 
     constructor(private http: HttpClient) { }
 
+
+    /**
+    * Sends and create Item at Server
+    * @param Item to send
+    * @returns Observable of POST-Method; Error 404 if failed.
+    */
     createItem(item: Item) {
         return this.http.post(environment.SERVER_URL + '/api/item', { item });
     }
 
+    /**
+    * Gets Item from Server
+    * @param ItemID to identify item
+    * @returns Observable with Item in data; Error 404 if item isnt found
+    */
     getItem(itemID: string) {
         return this.http.get<Item>(environment.SERVER_URL + '/api/item/' + itemID);
     }
 
+    /**
+    * Gets all Items from a user from the server
+    * @param UserID to identify user to fetch items from
+    * @returns Observable with Array of Items which are owned by user; Error 404 if item isnt found
+    */
     getItems(userID: string) {
         return this.http.get<Item[]>(environment.SERVER_URL + `/api/items/${userID}`);
     }
 
+    /**
+    * Delete Item on server
+    * @param ItemID of item which is deleted
+    * @returns Observable with response; Error 404 if failed
+    */
     deleteItem(itemID: string) {
         return this.http.delete(environment.SERVER_URL + '/api/item/' + itemID);
     }
 
+    /**
+    * Update Item on server
+    * @param Item with updated values
+    * @returns Observable with response; Error 404 if failed
+    */
     updateItem(item: Item) {
         return this.http.put(environment.SERVER_URL + '/api/item/', { item });
     }
 
+    /**
+    * Search for items, search based on name and tags
+    * @param Search-String to search for
+    * @returns Observable with response; Error 404 if failed
+    */
     searchItems(searchString: string) {
         return this.http.get<Item[]>(environment.SERVER_URL + '/api/search-items/' + searchString);
     }
 
+    /**
+    * Get all used tags of items
+    * @returns Observable with Array full of used tags; Error 404 if failed
+    */
     getTags() {
         return this.http.get<string[]>(environment.SERVER_URL + '/api/tags');
     }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,22 +10,46 @@ import { map } from 'rxjs/operators';
 export class UserService {
 	constructor(private http: HttpClient, private fireAuth: AngularFireAuth) {}
 
+
+	/**
+    * Registers a user
+    * @param user User to register
+	* @param password Password string to register with
+    * @returns Observable of POST-Method; Error 404 if failed.
+    */
 	register(user: User, password: string) {
 		return this.http.post(environment.SERVER_URL + '/api/register', { user, password });
 	}
-
+	/**
+    * Signing of a user
+    * @param email Email to signin with
+	* @param password Password string to signin with
+    * @returns Void.
+    */
 	signin(email: string, password: string) {
 		return from(this.fireAuth.auth.signInWithEmailAndPassword(email, password));
 	}
-
+	/**
+    * Signs out the current user
+    * @returns void.
+    */
 	signOut() {
 		return from(this.fireAuth.auth.signOut());
 	}
 
+	/**
+    * Fetch User from Server
+    * @param userID ID of User to search for
+	* @returns Observable with user; Error 404 if failed.
+    */
 	getUser(userID: string) {
 		return this.http.get<User>(environment.SERVER_URL + '/api/user/' + userID);
 	}
 
+	/**
+    * Fetch the current IDToken of user
+	* @returns Observable of IT-Token; Null if not registert.
+    */
 	getIdToken() {
 		if (this.fireAuth.auth.currentUser) {
 			return from(this.fireAuth.auth.currentUser.getIdToken());
@@ -34,10 +58,19 @@ export class UserService {
 		}
 	}
 
+	/**
+    * Check if user is authenticated
+    * @returns True, if user is authenticated, false if not.
+    */
 	isAuthenticated() {
 		return this.fireAuth.authState.pipe(map(user => (user ? true : false)));
 	}
 
+	/**
+    * Updates a user
+    * @param user User with updated values
+    * @returns Observable of PUT-Method; Error 404 if failed.
+    */
 	updateUser(user: User) {
 		return this.http.put(environment.SERVER_URL + '/api/update-user', { user });
 	}
