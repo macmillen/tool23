8af2fdb60fc167aa2680eba8524c7566f831e2f3
Christoph Peil <3v9_56sm2oe8hrl1@byom.de>
2019-08-01 19:17:47 +0200

    transactions commented

--- src/app/transaction/transaction-detail/transaction-detail.page.ts
+++ src/app/transaction/transaction-detail/transaction-detail.page.ts
@@ -11,15 +11,19 @@ import { TransactionService } from 'src/app/services/transaction.service';
     templateUrl: './transaction-detail.page.html',
     styleUrls: ['./transaction-detail.page.scss'],
 })
+
+/**
+ * Detailpage over a specific transaction
+ */
 export class TransactionDetailPage implements OnInit {
 
-    item: Item;
-    transaction: Transaction;
-    startDateString: string;
-    endDateString: string;
-    errorDate = false;
-    minYear = new Date().getFullYear();
-    maxYear = new Date().getFullYear() + 1;
+    item: Item; // Item of the transaction
+    transaction: Transaction;   // The transaction itself
+    startDateString: string;    // String of date when transaction starts
+    endDateString: string;      // String of date when transaction ends
+    errorDate = false;          // Bool Value if startDate is later than endDate
+    minYear = new Date().getFullYear(); // Time span start
+    maxYear = new Date().getFullYear() + 1; // Time span end
 
     constructor(
         private route: ActivatedRoute,
@@ -29,10 +33,16 @@ export class TransactionDetailPage implements OnInit {
         private navController: NavController,
     ) { }
 
+    /**
+     * Fetches the item ID
+     */
     ngOnInit() {
         this.getItemID();
     }
 
+    /**
+     * Fetches the item ID from the server
+     */
     private getItemID() {
         this.route.paramMap.subscribe(params => {
             const itemID = params.get('itemID');
@@ -40,6 +50,10 @@ export class TransactionDetailPage implements OnInit {
         });
     }
 
+    /**
+     * Get item from server based on itemID
+     * @param itemID ID of item to fetch
+     */
     getItem(itemID: string) {
         this.itemService.getItem(itemID).subscribe({
             next: item => {
@@ -59,7 +73,9 @@ export class TransactionDetailPage implements OnInit {
             error: e => console.log(e)
         });
     }
-
+    /**
+     * Creates Request to take item
+     */
     requestItem() {
         this.transactionService.requestItem(this.transaction, this.item._id).subscribe({
             next: async () => {
@@ -73,7 +89,11 @@ export class TransactionDetailPage implements OnInit {
             }
         });
     }
-
+    /**
+     * Sets date, based on value of event target
+     * @param event event to get date data from
+     * @param type specifies if start or end date
+     */
     setDate(event, type: 'start' | 'end') {
         const dateString = event.target.value;
 
@@ -87,6 +107,11 @@ export class TransactionDetailPage implements OnInit {
         }
     }
 
+    /**
+     * Formats date object to string
+     * @param date Date object to format
+     * @returns formated Date string
+     */
     toDateString(date: Date) {
         return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
     }
--- src/app/transaction/transaction-list/give-rating/give-rating.component.ts
+++ src/app/transaction/transaction-list/give-rating/give-rating.component.ts
@@ -9,11 +9,14 @@ import { AngularFireStorage } from '@angular/fire/storage';
   templateUrl: './give-rating.component.html',
   styleUrls: ['./give-rating.component.scss'],
 })
+/**
+ * Modal/Component to rate a transaction
+ */
 export class GiveRatingComponent implements OnInit {
 
-  comment = '';
-  rating = 0;
-  userImageURL = '../../../assets/placeholder.png';
+  comment = ''; // Variable for comment, starts empty
+  rating = 0;   // rating in points
+  userImageURL = '../../../assets/placeholder.png'; //Placeholder imageURL
   @Input() transaction: TransactionRequest;
   @Input() type: string;
 
@@ -23,10 +26,16 @@ export class GiveRatingComponent implements OnInit {
     private fireStorage: AngularFireStorage,
   ) { }
 
+  /**
+   * Fetch userImage on Init
+   */
   ngOnInit() {
     this.getUserImageURL();
   }
 
+  /**
+   * Rate the transaction based on set type
+   */
   rate() {
     if (this.type === 'taker') {
       this.rateTakerTransaction(this.transaction._id, this.comment, this.rating);
@@ -35,18 +44,33 @@ export class GiveRatingComponent implements OnInit {
     }
   }
 
+  /**
+   * Rate the Transaction as taker
+   * @param transactionID ID of the transaction
+   * @param comment Comment to the transaction
+   * @param rating Points given to the transaction
+   */
   rateTakerTransaction(transactionID: string, comment: string, rating: number) {
     this.transactionService.rateTakerTransaction(transactionID, comment, rating).subscribe({
       next: () => this.modalController.dismiss()
     });
   }
 
+/**
+   * Rate the Transaction as giver
+   * @param transactionID ID of the transaction
+   * @param comment Comment to the transaction
+   * @param rating Points given to the transaction
+   */
   rateGiverTransaction(transactionID: string, comment: string, rating: number) {
     this.transactionService.rateGiverTransaction(transactionID, comment, rating).subscribe({
       next: () => this.modalController.dismiss()
     });
   }
 
+  /**
+   * Fetch URL of user iamge from server and save it
+   */
   getUserImageURL() {
     const ref = this.fireStorage.ref(`profilepics/${this.transaction.user.userID}.jpg`);
     ref.getDownloadURL().subscribe({
